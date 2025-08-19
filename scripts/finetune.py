9be596fdeb90a9fc7b639c5591619ac5b8aa3611
roipaname <roipaname@gmail.com>
2025-08-19 23:24:26 +0200

    finished finetuner

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -59,3 +59,35 @@ data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False
 )
+"""
+Training arguments set up
+"""
+
+args = TrainingArguments(
+    output_dir=OUT_DIR,
+    num_train_epochs=2,
+    per_device_train_batch_size=2,
+    gradient_accumulation_steps=8,
+    learning_rate=2e-4,
+    lr_scheduler_type="cosine",
+    warmup_ratio=0.05,
+    weight_decay=0.01,
+    logging_steps=50,
+    save_steps=500,
+    save_total_limit=2,
+    bf16=False,
+    fp16=False,
+    report_to="none",
+)
+trainer=Trainer(
+    model=model,
+    args=args,
+    data_collator=data_collator,
+    train_dataset=tokenized
+)
+
+trainer.train()
+# 5) Save adapter + tokenizer
+trainer.save_model(OUT_DIR)
+tokenizer.save_pretrained(OUT_DIR)
+print(f"Saved LoRA adapter to {OUT_DIR}")

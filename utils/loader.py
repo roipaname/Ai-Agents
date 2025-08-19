adbfaa76a07f7809950bd55694b14a40bbc34c8d
roipaname <roipaname@gmail.com>
2025-08-19 21:58:48 +0200

    add

--- utils/loader.py
+++ utils/loader.py
@@ -3,3 +3,7 @@ from typing import List
 from pathlib import path
 from unstructured.partition.pdf import partition_pdf
 from unstructured.partition.docx import partition_docx
+
+def load_txt(path: str) -> str:
+    with open(path, "r", encoding="utf-8", errors="ignore") as f:
+        return f.read()
